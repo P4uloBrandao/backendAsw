f9dcf006cda0a302d24758fff79ab75e8425cb53
carlosbenoliel15 <57817588+carlosbenoliel15@users.noreply.github.com>
2023-05-23 13:54:41 +0100

    chat room

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const categoryRoutes=require('./routes/categoriasRoutes');
 const marcaRoutes=require('./routes/marcaRoutes')
-const chatRooms=require('./models/ChatRoom')
+const ChatRooms=require('./models/ChatRoom')
+const asyncHandler = require('express-async-handler')
 
 let messagesArray = [];
 let count;
@@ -17,6 +18,8 @@ let count;
 require("dotenv").config();
 
 const app = express();
+
+
 
 db.on('error', console.error.bind(console, 'Erro de conexão com MongoDB:'));
 
@@ -35,44 +38,59 @@ db.once('open', () => {
     console.log(`Servidor em execução na porta ${PORT}.`);
   });
   //chatRoom
-
-  const io = socket();
-  io.attach(server);
-
-  io.sockets.on('connection', (socket) => {
-      socket.on('join', (data) => {
-          socket.join(data.room);
-          chatRooms.find({}).toArray((err, rooms) => {
-              if(err){
-                  console.log(err);
-                  return false;
-              }
-              count = 0;
-              rooms.forEach((room) => {
-                  if(room.name == data.room){
-                      count++;
-                  }
-              });
-              if(count == 0) {
-                  chatRooms.insert({ name: data.room, messages: [] }); 
-              }
-          });
-      });
-      socket.on('message', (data) => {
-          io.in(data.room).emit('new message', {user: data.user, message: data.message});
-          chatRooms.update({name: data.room}, { $push: { messages: { user: data.user, message: data.message } } }, (err, res) => {
-              if(err) {
-                  console.log(err);
-                  return false;
-              }
-              console.log("Document updated");
-          });
-      });
-      socket.on('typing', (data) => {
-          socket.broadcast.in(data.room).emit('typing', {data: data, isTyping: true});
-      });
+  const io = require('socket.io')(server, {
+    cors: {
+      origin: 'http://localhost:4200',
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['my-custom-header'],
+      credentials: true
+    }
   });
 
+  io.on('connection', (socket) => {
+    socket.on('join', async (data) => {
+      socket.join(data.room);
+      try {
+        const rooms = await ChatRooms.find({ name: data.room });
+        let count = 0;
+        rooms.forEach((room) => {
+          if (room.name === data.room) {
+            count++;
+          }
+        });
+        if (count === 0) {
+          const chatRoom = new ChatRooms({ name: data.room, messages: [] });
+          const newChat = await chatRoom.save();
+          console.log('Chat room created');
+          console.log(newChat);
+        }
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    });
+  
+    socket.on('message', async (data) => {
+      io.in(data.room).emit('new message', { user: data.user, message: data.message });
+      try {
+        const updatedChatRoom = await ChatRooms.findOneAndUpdate(
+          { name: data.room },
+          { $push: { messages: { user: data.user, message: data.message } } },
+          { new: true }
+        );
+        console.log('Chat room updated');
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    });
+  
+    socket.on('typing', (data) => {
+      socket.broadcast.in(data.room).emit('typing', { data: data, isTyping: true });
+    });
+  });
+  
+  
   // Routes base:
   app.use('/users', userRoutes);
  // app.use('/products', productRoutes);
@@ -85,15 +103,25 @@ db.once('open', () => {
   app.post('/register', registerController.registerUser);
   app.post('/login', loginController.loginUser);
 
-  app.get('/chatroom/:room', (req, res, next) => {
-    let room = req.params.room;
-    chatRooms.find({name: room}).toArray((err, chatroom) => {
-        if(err) {
-            console.log(err);
-            return false;
-        }
-        res.json(chatroom[0].messages);
-    });
-  });
+
+  app.get('/chatroom/:room', asyncHandler(async (req, res) => {
+    try {
+      const { room } = req.params;
+      console.log("room")
+      console.log(room)
+
+      const chatRoom = await ChatRooms.findOne({ name: room });
+      console.log(chatRoom)
+      if (chatRoom) {
+        console.log(chatRoom.messages);
+        res.json(chatRoom.messages);
+      } else {
+        res.status(400).json("No chat found");
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  }));
+  
 
 });
